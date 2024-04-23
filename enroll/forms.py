b3e8f30595ae304e6d05f5af4e8572af59d537ed
Rajeev More <100182072+RajeevMore@users.noreply.github.com>
2024-04-23 17:35:54 +0530

    Update forms.py

--- enroll/forms.py
+++ enroll/forms.py
@@ -1,1 +1,12 @@
 
+from django.core import validators
+
+from django import forms
+from enroll.models import User
+
+class StudentRegistrationForm(forms.ModelForm):  
+    class Meta:
+        model = User
+        fields = ("name","email","password")
+        labels = {'name': 'Enter Name', 'email' : "Enter Email ", 'password':'Enter Password'}
+        widgets ={'password': forms.PasswordInput(render_value=True,)}
